0b3148fa8c71d7b32c7d19ce85c5b8bd59a5ade3
Manu Garcia <manugarciaes@gmail.com>
2018-07-20 16:35:07 +0200

    hash decorator working

--- lib/decorator/hash.rb
+++ lib/decorator/hash.rb
@@ -1,3 +1,5 @@
+require 'colorize'
+
 module PrintBeauty
   module Decorator
     class Hash
@@ -6,8 +8,33 @@ module PrintBeauty
         false
       end
 
-      def self.decorate(object)
+      def self.decorate(object, level: 1)
+        decorated = "#{tab((level))}{ \n".red
 
+        object.each do |key, value|
+          decorated += print_key(key, tab(level))
+          if value.is_a? ::Hash
+            decorated += decorate(value, level: level += 1)
+            next
+          end
+
+          decorated += print_value(value)
+        end
+        decorated += "#{tab((level))}} \n".red
+        decorated
+      end
+
+      def self.print_key(key, tabs)
+        "#{tabs}#{key} => ".red
+      end
+
+      def self.print_value(value)
+
+        "#{value.to_s.green}\n"
+      end
+
+      def self.tab(quantity)
+        "".rjust(quantity, "\t")
       end
     end
   end
--- spec/decorator/hash_spec.rb
+++ spec/decorator/hash_spec.rb
@@ -17,6 +17,17 @@ describe PrintBeauty::Decorator::Hash do
     expect(decorator.valid? hash).to be false
   end
 
-  it 'decorate' do
+  it 'decorate one level' do
+    hash = { name: 'test' }
+    puts decorator.decorate(hash)
+    expect(decorator.decorate(hash)).to eq '"\e[0;31;49m { \n\e[0m\e[0;31;49m\tname => \e[0m\e[0;32;49mtest\e[0m\n\e[0;31;49m } \n\e[0m"'
   end
+
+  it 'decorate two level' do
+    hash = { name: 'test', level: { surname: 'garcia'} }
+    puts decorator.decorate(hash)
+    byebug
+    expect(decorator.decorate(hash)).to eq '"\e[0;31;49m { \n\e[0m\e[0;31;49m\tname => \e[0m\e[0;32;49mtest\e[0m\n\e[0;31;49m } \n\e[0m"'
+  end
+
 end
